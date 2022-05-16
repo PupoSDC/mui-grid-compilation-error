4492f5152672287c8a7146e951c279a0a16cbb6e
Pedro Pupo Sa da Costa <pedro@hublo.com>
2022-05-16 13:41:43 +0100

    remove useless configs and add readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const rows: GridRowsProp = [
   { id: 1, col1: "Hello", col2: "World" },
   { id: 2, col1: "XGrid", col2: "is Awesome" },
   { id: 3, col1: "Material-UI", col2: "is Amazing" },
-];
+]
 
 const columns: GridColDef[] = [
   { field: "id", hide: true },
